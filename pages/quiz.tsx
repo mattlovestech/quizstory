af3088b55ba02b3318818e16c8e54e2b888d3d1a
Matthew Manning <matthewmanning@apple.com>
2022-02-09 16:12:54 -0500

    added four quiz buttons

--- pages/quiz.tsx
+++ pages/quiz.tsx
@@ -23,34 +23,49 @@ export default function Quiz() {
                    justifyContent="space-between"
                    alignItems="center" spacing={2}>
                <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
-               <a>Insert Quiz Name</a>
+               <h3>Insert Quiz Name</h3>
                <MoreHorizIcon/>
            </Stack>
+           <br/>
            <div  style={{textAlign: "center"}}>
                <img height="200px"
-                    style={{marginBottom: "25px"}}
+
                     alt="hbcu gif text"
                     src={"https://media1.giphy.com/media/rcTvOttwC4wDVn5G1t/giphy.gif?cid=ecf05e47hstgngv6j76gb6gg1c55v3dg9r7pfknsj7pvchoq&rid=giphy.gif&ct=s"}/>
            </div>
+           <br/>
            <div style={{marginLeft: "10%", marginRight: "10%"}}>
            <Box style={{color: "white", padding: "10px",textAlign: "center", background: "black", borderTopLeftRadius: "25px", borderTopRightRadius: "25px"}}>
                <h3>Insert Question...
                </h3>
            </Box>
            <Box style={{textAlign: "center", background: "white", borderBottomLeftRadius: "25px", borderBottomRightRadius: "25px"}}>
-           <Button variant={"contained"} style={{margin: "10px"}}>
-               <Stack  direction="row" justifyContent="space-between" alignItems="center" spacing={2}>
+           <Button variant={"contained"} style={{margin: "10px", width: "75%"}}>
+               <Stack  direction="row"  alignItems="center" spacing={2}>
                <Avatar sx={{ width: 24, height: 24 }} alt="A" src="/static/images/avatar/1.jpg" />
-                   <a>Answer....</a>
+                   <a>Answer.....</a>
                </Stack>
            </Button>
-               <br/>
-               <Button variant={"contained"}>
-                   <Stack  direction="row" justifyContent="space-between" alignItems="center" spacing={2}>
+
+               <Button variant={"contained"} style={{margin: "10px", width: "75%"}}>
+                   <Stack  direction="row"  alignItems="center" spacing={2}>
                        <Avatar sx={{ width: 24, height: 24 }} alt="A" src="/static/images/avatar/1.jpg" />
-                       <a>Answer....</a>
+                       <a>Answer.....</a>
                    </Stack>
                </Button>
+               <Button variant={"contained"} style={{margin: "10px", width: "75%"}}>
+                   <Stack  direction="row"  alignItems="center" spacing={2}>
+                       <Avatar sx={{ width: 24, height: 24 }} alt="A" src="/static/images/avatar/1.jpg" />
+                       <a>Answer.....</a>
+                   </Stack>
+               </Button>
+               <Button variant={"contained"} style={{margin: "10px", width: "75%"}}>
+                   <Stack  direction="row"  alignItems="center" spacing={2}>
+                       <Avatar sx={{ width: 24, height: 24 }} alt="A" src="/static/images/avatar/1.jpg" />
+                       <a>Answer.....</a>
+                   </Stack>
+               </Button>
+               <br/> <br/>
 
        </Box>
            </div>
