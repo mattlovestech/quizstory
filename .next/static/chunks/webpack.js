13d89b67682522d7ef723ea3034c5c9c7a5f4b68
Matthew Manning <matthewmanning@apple.com>
2022-02-09 15:04:43 -0500

    added components to quiz page

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "ced2ce3e2beb7044"; }
+/******/ 		__webpack_require__.h = function() { return "7058b3851316536a"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/quiz.tsx
+++ pages/quiz.tsx
@@ -4,10 +4,17 @@ import {Stack} from "@mui/material";
 import Avatar from "@mui/material/Avatar";
 import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
 import React from "react";
+import Image from "next/image";
+import {textAlign} from "@mui/system";
+import Box from "@mui/material/Box";
+import Header from "../components/header";
+import Button from "@mui/material/Button";
 
 export default function Quiz() {
     return (
+
        <>
+
            <br/>
            <br/>
        <ProgressTopBar/>
@@ -16,9 +23,31 @@ export default function Quiz() {
                    justifyContent="space-between"
                    alignItems="center" spacing={2}>
                <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
-               <a>Quiz Name</a>
+               <a>Insert Quiz Name</a>
                <MoreHorizIcon/>
            </Stack>
+           <div  style={{textAlign: "center"}}>
+               <img height="200px"
+                    style={{marginBottom: "25px"}}
+                    alt="hbcu gif text"
+                    src={"https://media1.giphy.com/media/rcTvOttwC4wDVn5G1t/giphy.gif?cid=ecf05e47hstgngv6j76gb6gg1c55v3dg9r7pfknsj7pvchoq&rid=giphy.gif&ct=s"}/>
+           </div>
+           <div>
+               <Stack style={{backgroundColor: "white",
+                   textAlign: "center",
+               borderRadius: "25px"}} spacing={2}>
+                   <h3 style={{background: "black", borderRadius: "25px"}}>
+                       Insert Question?<br/>
+                       ...<br/>
+                       ...
+                   </h3>
+                   <a><Button>Anser....</Button></a>
+                   <a>Item 2</a>
+                   <a>Item 3</a>
+                   <a>Item 4</a>
+               </Stack>
+           </div>
        </>
+
     )
 }
