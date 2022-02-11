dbb062ccc0c8a5f918cb762a514ada4748f02ab0
Matthew Manning <matthewmanning@apple.com>
2022-02-10 22:02:17 -0500

    removed the styles import

--- components/progress.tsx
+++ components/progress.tsx
@@ -1,7 +1,6 @@
 
 import Box from '@mui/material/Box';
 import React, { Component } from 'react';
-import { withStyles } from '@material-ui/core/styles';
 import { LinearProgress } from '@material-ui/core';
 
 export default function ProgressTopBar(props: any) {
